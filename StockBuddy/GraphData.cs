48d5eb1e40bc9880fc95443ac62b4f3dcfc48cb7
Arbaaz Meghani <amegha3@uic.edu>
2018-02-15 18:33:32 -0600

    Added classes to get overall statistics and the statistics based on the stock symbol

---
+++ StockBuddy/GraphData.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+public class GraphData
+{
+    private HttpClient httpClient;
+    private static GraphData graphData;
+
+    private GraphData()
+    {
+        httpClient = new HttpClient();
+    }
+
+    private async Task<String> getRequest(String url)
+    {
+        HttpResponseMessage httpResponseMessage = await httpClient.GetAsync(url);
+        HttpContent httpContent = httpResponseMessage.Content;
+        return await httpContent.ReadAsStringAsync();
+    }
+
+    private String formatURL(String symbol, String type)
+    {
+        String base_url;
+        if (type.Equals("1d"))
+            base_url = "https://api.iextrading.com/1.0/stock/{0}/chart/{1}?filter=date,label,average&format=csv";
+        else
+            base_url = "https://api.iextrading.com/1.0/stock/{0}/chart/{1}?filter=date,close&format=csv";
+        return String.Format(base_url, symbol, type);
+    }
+
+    public String fiveYears(String symbol)
+    {
+        String url = formatURL(symbol, "5y");
+        return getRequest(url).Result;
+    }
+
+    public String year(String symbol)
+    {
+        String url = formatURL(symbol, "1y");
+        return getRequest(url).Result;
+    }
+
+    public String halfYear(String symbol)
+    {
+        String url = formatURL(symbol, "6m");
+        return getRequest(url).Result;
+    }
+
+    public String quarter(String symbol)
+    {
+        String url = formatURL(symbol, "3m");
+        return getRequest(url).Result;
+    }
+
+    public String month(String symbol)
+    {
+        String url = formatURL(symbol, "1m");
+        return getRequest(url).Result;
+    }
+
+    public String intraday(String symbol)
+    {
+        String url = formatURL(symbol, "1d");
+        return getRequest(url).Result;
+    }
+
+    public static GraphData getGraphData()
+    {
+        if (graphData == null)
+            graphData = new GraphData();
+        return graphData;
+    }
+}
---
+++ StockBuddy/Statistics.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+public class Statistics
+{
+    private HttpClient httpClient;
+    private static Statistics stats;
+
+    private Statistics()
+    {
+        httpClient = new HttpClient();
+    }
+
+    private async Task<String> getRequest(String url)
+    {
+        HttpResponseMessage httpResponseMessage = await httpClient.GetAsync(url);
+        HttpContent httpContent = httpResponseMessage.Content;
+        return await httpContent.ReadAsStringAsync();
+    }
+
+    private String formatURL(String symbol, String type)
+    {
+        String base_url = "https://api.iextrading.com/1.0/stock/{0}/{1}/{2}&format=csv";
+        String filter = "";
+        if (type.Equals("quote"))
+            filter = "?filter=open,high,low,latestVolume,avgTotalVolume,peRatio,week52High,week52Low";
+        else
+            filter = "?filter=marketcap,dividendYield";
+        return String.Format(base_url, symbol, type, filter);
+    }
+
+    public String statistics(String symbol)
+    {
+        String url1 = formatURL(symbol, "quote");
+        String url2 = formatURL(symbol, "stats");
+        return getRequest(url1).Result + "\n\n" +getRequest(url2).Result;
+    }
+
+    public static Statistics getStatistics()
+    {
+        if (stats == null)
+            stats = new Statistics();
+        return stats;
+    }
+}
