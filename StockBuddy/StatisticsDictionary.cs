6b62a8a546feb55bac081b05469381b552817af5
Arbaaz Meghani <amegha3@uic.edu>
2018-02-19 17:02:40 -0600

    Added class to parse statistics data and a class to get the all the stock symbols.

---
+++ StockBuddy/StatisticsDictionary.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class StatisticsDictionary
+{
+    private Dictionary<String, String> statistics;
+    String symbol;
+
+    public StatisticsDictionary(String symbol)
+    {
+        statistics = new Dictionary<string, string>();
+        this.symbol = symbol;
+        retrieveStatistics();
+    }
+
+    private void retrieveStatistics()
+    {
+        Tuple<String, String> statStrings = Statistics.getStatistics().statistics(symbol);
+        addToDictionary(statStrings.Item1);
+        addToDictionary(statStrings.Item2);
+    }
+
+    private void addToDictionary(String data)
+    {
+        String[] splitData = data.Split('\n');
+        String[] dataOnly = splitData[1].Split(',');
+        String[] headersOnly = splitData[0].Split(',');
+        for (int i = 0; i < dataOnly.Length; i++)
+            statistics.Add(headersOnly[i], dataOnly[i]);
+    }
+
+    public Dictionary<String, String>.KeyCollection allKeywords()
+    {
+        return statistics.Keys;
+    }
+
+    public String getStat(String keyword)
+    {
+        try
+        {
+            return statistics[keyword];
+        }
+        catch(Exception e)
+        {
+            return "Illegal keyword";
+        }
+    }
+}
---
+++ StockBuddy/StockSymbols.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+
+class StockSymbols
+{
+    private HttpClient httpClient;
+    private static StockSymbols stockSymbols;
+    private static HashSet<String> allSymbols;
+
+    private StockSymbols()
+    {
+        httpClient = new HttpClient();
+        allSymbols = new HashSet<string>();
+        requestAllSymbols();
+    }
+
+    private void requestAllSymbols()
+    {
+        String symbols = getRequest().Result;
+        String[] individualSymbols = symbols.Split('\n');
+        for (int i = 1; i < individualSymbols.Length; i++)
+            allSymbols.Add(individualSymbols[i]);
+    }
+
+    private async Task<String> getRequest()
+    {
+        String url = "https://api.iextrading.com/1.0/ref-data/symbols?filter=symbol&format=csv";
+        HttpResponseMessage httpResponseMessage = await httpClient.GetAsync(url);
+        HttpContent httpContent = httpResponseMessage.Content;
+        return await httpContent.ReadAsStringAsync();
+    }
+
+    public static HashSet<String> getAllSymbols()
+    {
+        if (allSymbols == null)
+            stockSymbols = new StockSymbols();
+        return allSymbols;
+    }
+}
