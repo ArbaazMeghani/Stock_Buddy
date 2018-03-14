998b40eacce9f63666dfe0d5136aaef05348913f
Arbaaz Meghani <amegha3@uic.edu>
2018-03-13 19:12:58 -0500

    Added boilerplate code for saving data

---
+++ StockBuddy/Profile.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class Profile
+{
+    public Profile()
+    {
+
+    }
+
+    public void SaveWatchListItem(String symbol)
+    {
+
+    }
+
+    public void DeleteWatchListItem(String symbol)
+    {
+
+    }
+
+    public List<String> RetrieveWatchList()
+    {
+        return null;
+    }
+
+    public void SavePurchase(String symbol, int quantity, double price)
+    {
+
+    }
+
+    public Tuple<String, int, double> RetrievePurchases()
+    {
+        return null;
+    }
+}
