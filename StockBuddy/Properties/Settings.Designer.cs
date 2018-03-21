5b24f21d086dab19a67f5a8e16ad76c42e5f201e
pedrocardenas <pcarde2@uic.edu>
2018-03-20 19:27:35 -0500

    Data Base

--- StockBuddy/Properties/Settings.Designer.cs
+++ StockBuddy/Properties/Settings.Designer.cs
@@ -46,5 +46,16 @@ namespace StockBuddy.Properties {
                 this["FirstTime"] = value;
             }
         }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\WatchList.mdf" +
+            ";Integrated Security=True")]
+        public string WatchListConnectionString {
+            get {
+                return ((string)(this["WatchListConnectionString"]));
+            }
+        }
     }
 }
