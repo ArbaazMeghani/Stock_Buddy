644b3e7e7f17ceaafbe6e9181e751f849f8692dd
Arbaaz Meghani <amegha3@uic.edu>
2018-03-22 15:53:42 -0500

    Finished database manager class and updated profile class to call the functions with the queries and return any data that needs to be returned

--- StockBuddy/DatabaseManager.cs
+++ StockBuddy/DatabaseManager.cs
@@ -9,16 +9,48 @@ using System.Globalization;
 
 class DatabaseManager
 {
-    private const String CONN_STRING = "Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=./WatchList.mdf;Integrated Security = True";
+    private const String CONN_STRING = "Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=.\WatchList.mdf;Integrated Security = True";
 
     public DatabaseManager() { }
 
     private SqlCommand Connect(String query)
     {
         SqlConnection sqlConnection = new SqlConnection(CONN_STRING);
-        SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+        SqlCommand command = new SqlCommand(query, sqlConnection);
         sqlConnection.Open();
-        return sqlCommand;
+        return command;
+    }
+
+    public void SimpleQueryExecution(String query)
+    {
+        SqlCommand command = Connect(query);
+        NonQuery(command);
+    }
+
+    public void UpdateTableItem(String symbol, String query)
+    {
+        SqlCommand command = Connect(query);
+        command.Parameters.AddWithValue("@Symbol", symbol);
+        NonQuery(command);
+    }
+
+    public void UpdatePurchaseListItem(String symbol, int quantity, double price, String query)
+    {
+        SqlCommand command = Connect(query);
+        command.Parameters.AddWithValue("@Symbol", symbol);
+        command.Parameters.AddWithValue("@Quantity", quantity);
+        command.Parameters.AddWithValue("@Price", price);
+        NonQuery(command);
+    }
+
+    public DataTable SelectQuery(String query)
+    {
+        DataTable dataTable = new DataTable();
+        SqlCommand command = Connect(query);
+        SqlDataAdapter dataAdapter = new SqlDataAdapter(command);
+        dataAdapter.Fill(dataTable);
+        Disconnect(command.Connection);
+        return dataTable;
     }
 
     private void Disconnect(SqlConnection connection)
@@ -26,9 +58,8 @@ class DatabaseManager
         connection.Close();
     }
 
-    private void ScalarQuery(String query)
+    private void NonQuery(SqlCommand command)
     {
-        SqlCommand command = Connect(query);
         command.ExecuteNonQuery();
         Disconnect(command.Connection);
     }
--- StockBuddy/Profile.cs
+++ StockBuddy/Profile.cs
@@ -8,60 +8,69 @@ using System.Data;
 
 class Profile
 {
+    private DatabaseManager databaseManager;
+
     public Profile()
     {
-
+        databaseManager = new DatabaseManager();
     }
 
     public void ClearProfile()
     {
         String clearWatchListQuery = "DELETE FROM WatchList;";
         String clearPurhcaseListQuery = "DELETE FROM PurchaseList;";
+        databaseManager.SimpleQueryExecution(clearPurhcaseListQuery);
+        databaseManager.SimpleQueryExecution(clearWatchListQuery);
     }
 
     public void SaveWatchListItem(String symbol)
     {
         String addWatchListItemQuery = "INSERT INTO WatchList (Symbol) VALUES (@Symbol);";
+        databaseManager.UpdateTableItem(symbol, addWatchListItemQuery);
     }
 
     public void DeleteWatchListItem(String symbol)
     {
         String deleteWatchListItemQuery = "DELETE FROM WatchList WHERE Symbol =  @Symbol;";
+        databaseManager.UpdateTableItem(symbol, deleteWatchListItemQuery);
     }
 
     public List<String> RetrieveWatchList()
     {
         String query = "SELECT * FROM WatchList;";
-        DataTable watchTable = null; //TODO
+        DataTable watchTable = databaseManager.SelectQuery(query);
         List<String> watchList = new List<string>();
         DataTableReader dataTableReader = watchTable.CreateDataReader();
         while(dataTableReader.Read())
         {
             watchList.Add(dataTableReader[0].ToString());
         }
-
+        watchTable.Dispose();
         return watchList;
     }
 
     public void SavePurchase(String symbol, int quantity, double price)
     {
         String addPurchaseListItemQuery = "INSERT INTO PurchaseList (Symbol,Quantity,Price) VALUES (@Symbol,@Quantity,@Price);";
+        databaseManager.UpdatePurchaseListItem(symbol, quantity, price, addPurchaseListItemQuery);
     }
 
     public void UpdatePurchase(String symbol, int quantity, double price)
     {
         String updatePurchaseListItemQuery = "UPDATE PurchaseList SET Quantity = @Quantity WHERE Symbol = @Symbol AND Price = @Price;";
+        databaseManager.UpdatePurchaseListItem(symbol, quantity, price, updatePurchaseListItemQuery);
     }
 
     public void DeletePurchase(String symbol)
     {
-        String deletePurchaseListItemQuery = "DELETE FROM PurchaseList WHERE Symbol =  @Symbol;";
+        String deletePurchaseListItemQuery = "DELETE FROM PurchaseList WHERE Symbol = @Symbol;";
+        databaseManager.UpdateTableItem(symbol, deletePurchaseListItemQuery);
     }
 
     public List<Tuple<String, int, double>> RetrievePurchases()
     {
         String query = "SELECT * FROM PurchaseList;";
-        DataTable purchaseTable = null; //TODO
+        DataTable purchaseTable = databaseManager.SelectQuery(query);
         List<Tuple<String, int, double>> purchaseList = new List<Tuple<string, int, double>>();
         DataTableReader dataTableReader = purchaseTable.CreateDataReader();
         while(dataTableReader.Read())
@@ -73,7 +82,7 @@ class Profile
 
             }
         }
-
+        purchaseTable.Dispose();
         return purchaseList;
     }
 }
