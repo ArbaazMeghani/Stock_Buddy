19fdd2e884097db3b63b2119dfcc5af42772e969
Arbaaz Meghani <amegha3@uic.edu>
2018-03-22 11:46:42 -0500

    Added database manager class for handling execution of queries and managing connections

---
+++ StockBuddy/DatabaseManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Globalization;
+
+class DatabaseManager
+{
+    private const String CONN_STRING = "Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=./WatchList.mdf;Integrated Security = True";
+
+    public DatabaseManager() { }
+
+    private SqlCommand Connect(String query)
+    {
+        SqlConnection sqlConnection = new SqlConnection(CONN_STRING);
+        SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+        sqlConnection.Open();
+        return sqlCommand;
+    }
+
+    private void Disconnect(SqlConnection connection)
+    {
+        connection.Close();
+    }
+
+    private void ScalarQuery(String query)
+    {
+        SqlCommand command = Connect(query);
+        command.ExecuteNonQuery();
+        Disconnect(command.Connection);
+    }
+}
